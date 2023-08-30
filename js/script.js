4035c118091da13f193ce083ccf32f7d13f9822d
ron-wollschlaeger <ron.wollschlaeger@frontshape.de>
2023-08-30 08:57:02 +0200

    Upload Challenge

---
+++ js/script.js
@@ -0,0 +1,14 @@
+module.exports = {
+    theme: {
+        extend: {
+            backgroundImage: {
+                'books': "url(https://images.unsplash.com/photo-1524995997946-a1c2e315a42f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80)",
+            }
+        },
+        fontFamily: {
+            'title': 'Marck Script, cursive',
+            'body': 'Hind, sans-serif',
+        },
+    },
+    plugins: [],
+}
